e0b5be6643586fb7ffa73a4fd674995c98955643
nazarukiv <nazaruk7649@ukr.net>
2026-01-16 16:10:22 +0000

    options menu added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class Main{
+
+    public static int rows=3;
+    public static int columns=4;
+    public static int amountOfEnemies = 10;
+    public static int transistorsNeeded = 100;
+    public static int moves = 40;
+
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
         String command;
 
         do {
-            System.out.println("Welcome to Java Game v 1.0. Please make your choice and press 'Enter':");
+            System.out.println("Welcome to Java Game v 1.0. Please make your choice and press 'Enter'.");
             System.out.println("1: start new game");
             System.out.println("2: options");
             System.out.println("3: credits");
@@ -20,14 +27,14 @@ public class Main{
                     startNewGame();
                     break;
                 case "2":
-                    openOptionsMenu();
+                    OptionsMenu.showOptionsMenu(scanner);
                     break;
                 case "3":
                     showCredits();
                     break;
                 case "4":
                     break;
-                    
+
                 default:
                     System.out.println("Command not recognised! Please try again");
             }
@@ -47,9 +54,6 @@ public class Main{
                 "version 1.0, last modified on ???\n" +
                 "contact me at nazaruk7649@ukr.net\n"+
                 "__________________________________________________________\n");
-    }
-
-    private static void openOptionsMenu() {
     }
 
 }
---
+++ src/OptionsMenu.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class OptionsMenu {
+    static int command;
+
+    public static void showOptionsMenu(Scanner scanner){
+        do{
+            System.out.println("Select your choice and press 'Enter'\n"+"" +
+                    "1: Show current settings\n"+
+                    "2: Change settings\n"+
+                    "3: Exit");
+            command = scanner.nextInt();
+
+            switch (command){
+                case 1:
+                    System.out.println("__________________________________________________________\n" +
+                            "\n Current settings:\n"+
+                            "rows: " + Main.rows+"\n"+
+                            "columns: " + Main.columns+"\n"+
+                            "enemies: " + Main.amountOfEnemies+"\n"+
+                            "transistors: " + Main.transistorsNeeded+"\n"+
+                            "moves: " + Main.moves+"\n"+
+                            "__________________________________________________________\n");
+                    break;
+                case 2:
+                    System.out.println("Enter a new value for rows: ");
+                    Main.rows = scanner.nextInt();
+                    System.out.println("Enter a new value for columns: ");
+                    Main.columns = scanner.nextInt();
+                    System.out.println("Enter a new value for enemies: ");
+                    Main.amountOfEnemies = scanner.nextInt();
+                    System.out.println("Enter a new value for transistors: ");
+                    Main.transistorsNeeded= scanner.nextInt();
+                    System.out.println("Enter a new value for moves: ");
+                    Main.moves = scanner.nextInt();
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("command not recognised, please try again!");
+                    break;
+
+            }
+        }
+        while(command!=3);
+    }
+}
